accb22a6d276ceac4241b93d24e955203490beb8
rafitrhmn <106608789+rafitrhmn@users.noreply.github.com>
2023-01-10 00:18:20 +0700

    menghapus file universitas github

--- universitas/delmk.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$database = new mysqli('localhost','root','','universitas');
-	if($database->connect_errno){
-		echo"Database Tidak Dapat Terhubung";
-	}
-	$sql = "DELETE FROM matakuliah where id_matakuliah =('$_GET[id_matakuliah]')";
-	$data=$database->query($sql);
-	header("location:index.php?page=2");
-	?>
--- universitas/delprodi.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$database = new mysqli('localhost','root','','universitas');
-	if($database->connect_errno){
-		echo"Database Tidak Dapat Terhubung";
-	}
-	$sql = "DELETE FROM prodi where id_prodi =('$_GET[id_prodi]')";
-	$data=$database->query($sql);
-	header("location:index.php?page=4");
-	?>
